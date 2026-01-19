dc40108a0c6cd8d1577efc393ac99477b21d6999
botir-nematilloyev <nematilloyevbro@gmail.com>
2026-01-19 16:05:07 +0500

    other migrations

---
+++ migrations/20260119150046_user_action.down.sql
@@ -0,0 +1,5 @@
+
+DROP INDEX IF EXISTS "user_actions_leave_branch_id_idx";
+DROP INDEX IF EXISTS "user_actions_visit_branch_id_idx";
+DROP INDEX IF EXISTS "user_actions_user_id_idx";
+DROP TABLE IF EXISTS "user_actions";
